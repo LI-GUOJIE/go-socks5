f0f4080e64a335856883f163915342cb2ada63df
EGG_PC\egg <imliguojie@gmail.com>
2023-06-30 17:20:06 +0800

    优化便利性

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # source /home/opc/.bashrc
-cd /home/admin/vpn/goss5
+MYDIR=/root/go-socks5-deepers
+cd  $MYDIR
 go build ss5
 
 while :
 do
-  pids_num=`netstat -lnp |grep ':::1080 ' | wc -l`
+  pids_num=`netstat -lnp |grep ':::23333' | wc -l`
   if [ $pids_num -eq 0 ]; then
     echo "" > log.file
     echo `date`"... pid_num: $pids_num" >> log.file
@@ -15,7 +16,7 @@ do
     whoami >> log.file
 
     # Run
-    nohup /home/admin/vpn/goss5/ss5 > /dev/null &
+    nohup $MYDIR/ss5 > /dev/null &
   fi
 
   sleep 5
--- main.go
+++ main.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"log"
-	"os"
 	"ss5/socks5"
 )
 
 func main() {
 
-	// Create a socks server
-	creds := socks5.StaticCredentials{
-		"egg": "change_to_what_you_like",
-	}
-	cator := socks5.UserPassAuthenticator{Credentials: creds}
-	conf := &socks5.Config{
-		AuthMethods: []socks5.Authenticator{cator},
-		Logger:      log.New(os.Stdout, "", log.LstdFlags),
-	}
-
+	// // Create a socks server
+	// creds := socks5.StaticCredentials{
+	// 	"egg": "change_to_what_you_like",
+	// }
+	// cator := socks5.UserPassAuthenticator{Credentials: creds}
+	// conf := &socks5.Config{
+	// 	AuthMethods: []socks5.Authenticator{cator},
+	// 	Logger:      log.New(os.Stdout, "", log.LstdFlags),
+	// }
+	conf := &socks5.Config{}
 	server, err := socks5.New(conf)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create SOCKS5 proxy on localhost port 1080
-	if err := server.ListenAndServe("tcp", "0.0.0.0:1080"); err != nil {
+	// Create SOCKS5 proxy on localhost port 23333
+	if err := server.ListenAndServe("tcp", "0.0.0.0:23333"); err != nil {
 		panic(err)
 	}
 }
